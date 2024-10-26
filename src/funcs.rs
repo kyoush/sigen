70ddd5cad560b9075a0b339740347404405d2521
Kyoshiro Kamura <reallove1970@gmail.com>
2024-10-26 11:12:58 +0900

    :recycle: linear taper

--- src/funcs.rs
+++ src/funcs.rs
@@ -37,20 +37,28 @@ pub fn generate_white_noise(amplitude: f64, duration: u32) -> Vec<f64> {
     samples
 }
 
-pub fn apply_linear_taper(samples: &mut [f64], taper_size: usize) {
-    let total_samples = samples.len();
-
-    let taper_size = taper_size.min(total_samples / 2);
-
+fn apply_linear_fade_in (samples: &mut [f64], taper_size: usize) {
     for i in 0..taper_size {
         let factor = i as f64 / taper_size as f64;
         samples[i] *= factor;
     }
+}
+
+fn apply_linear_fade_out (samples: &mut [f64], taper_size: usize) {
+    let total_samples = samples.len();
 
     for i in 0..taper_size {
         let factor = 1.0 - (taper_size - i) as f64 / taper_size as f64;
         samples[total_samples - 1 - i] *= factor;
     }
+}
+
+pub fn apply_linear_taper(samples: &mut [f64], taper_size: usize) {
+    let total_samples = samples.len();
+    let taper_size = taper_size.min(total_samples / 2);
+
+    apply_linear_fade_in(samples, taper_size);
+    apply_linear_fade_out(samples, taper_size);
 }
 
 pub fn write_wav_file(
