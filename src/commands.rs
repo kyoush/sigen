e53a3f9f889dcae190aacd53960f59b73d2bdd87
Kyoshiro Kamura <reallove1970@gmail.com>
2024-11-01 22:46:02 +0900

    :recycle: commands

---
+++ src/commands.rs
@@ -0,0 +1,37 @@
+use clap::{Parser, Subcommand};
+
+pub mod common;
+pub mod sine;
+pub mod white;
+pub mod tsp;
+
+pub const AMP_DEF: f64 = 0.45;
+pub const D_DEF_LONG: u32 = 30;        // sec
+pub const D_DEF_SHORT: u32 = 5;        // sec
+pub const FREQ_DEF: u32 = 440;     // Hz
+pub const LOW_FREQ_TSP_DEF: i32 = 20;  // Hz
+pub const HIGH_FREQ_TSP_DEF: i32 = 16_000; // Hz
+pub const FS_DEF: u32 = 44_100;        // Hz
+pub const LEN_TAPER_DEF: usize = 4096; //points
+
+/// A tool for generating WAV files of various signal types.
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+pub struct Cli {
+    #[command(subcommand)]
+    pub subcommand: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+pub enum Commands {
+    /// generate a wav file with a sine wave
+    Sine(sine::SineOptions),
+
+    /// generate a wave file with a white noise
+    White(white::WhiteOptions),
+
+    /// generate a wave file with a TSP [Time Stretched Pulse] waveform
+    Tsp (tsp::TspOptions),
+
+    // Wav(wav::WavOptions), // To be Extended
+}
---
+++ src/commands/sine.rs
@@ -0,0 +1,24 @@
+use clap::Args;
+
+#[derive(Args, Debug)]
+pub struct SineOptions {
+    /// Frequency of the sine wave in Hz
+    #[arg(
+        short, long,
+        default_value_t = super::FREQ_DEF,
+    )]
+    pub frequency: u32,
+
+    /// duration of the signal in seconds.
+    #[arg(
+        short, long,
+        default_value_t = super::D_DEF_LONG,
+    )]
+    pub duration: u32,
+
+    #[command(flatten)]
+    pub options: super::common::CommonOptions,
+
+    #[command(flatten)]
+    pub taper_opt: super::common::TaperSpec,
+}
