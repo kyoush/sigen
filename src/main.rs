e884b3015c858bbec91c85b493e0a246c14a892b
Kyoshiro Kamura <reallove1970@gmail.com>
2024-11-30 15:02:23 +0900

    :recycle: refactor main

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,9 @@ mod processing;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     match commands::Cli::parse().subcommand {
-        commands::Commands::Gen(opt) => { processing::signal_generator(&opt) }
-        commands::Commands::Taper(opt) => { processing::apply_taper_to_wav(&opt) }
-        commands::Commands::Wav(opt) => { processing::cat_wav_files(&opt) }
-        commands::Commands::Mod(opt) => { processing::wav_modurate(&opt) }
+        commands::Commands::Gen(opt) => processing::signal_generator(&opt),
+        commands::Commands::Taper(opt) => processing::apply_taper_to_wav(&opt),
+        commands::Commands::Wav(opt) => processing::cat_wav_files(&opt),
+        commands::Commands::Mod(opt) => processing::wav_modurate(&opt),
     }
 }
